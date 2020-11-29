0972afd9b9e801c825e6597c69f7be7714945a06
Harita Ravindranath <harrysama01@outlook.com>
2020-11-29 21:03:45 +0530

    sql file

--- src/main/resources/sql_setup2.sql
+++ src/main/resources/sql_setup2.sql
@@ -6,6 +6,43 @@ values ('admin','admin');
 
 
 Insert into flight_details (flightNo,airline,source_place,destination_place,departure,arrival,duration,duration_in_min,airportName,totalStops,availableSeats,fare
+) values ('GA-124','GoAir.','Mumbai','Delhi','13:16:38','17:50:01','4 h 33 min','273','Mumbai Airport','0','25','1797');
+Insert into flight_details (flightNo,airline,source_place,destination_place,departure,arrival,duration,duration_in_min,airportName,totalStops,availableSeats,fare
+) values ('SJ-124','SpiceJet.','Lucknow','Goa','12:38:06','12:38:06','4 h 46 min','286','Lucknow Airport','0','12','7164');
+Insert into flight_details (flightNo,airline,source_place,destination_place,departure,arrival,duration,duration_in_min,airportName,totalStops,availableSeats,fare
+) values ('SAS-124','Saudi Airlines saudia','Thiruvananathapuram','Bangalore','12:58:11','17:14:55','4 h 16 min','256','Thiruvananathapuram Airport','1','9','3812');
+Insert into flight_details (flightNo,airline,source_place,destination_place,departure,arrival,duration,duration_in_min,airportName,totalStops,availableSeats,fare
+) values ('AI-124','Air India.','Kolkata','Guwahati','12:24:09','18:28:05','6 h 3 min','363','Kolkata Airport','0','7','7193');
+
+Insert into flight_details (flightNo,airline,source_place,destination_place,departure,arrival,duration,duration_in_min,airportName,totalStops,availableSeats,fare
+) values ('EA-124','Etihad Airways','Varanasi','Ahmedabad','14:25:15','17:45:28','3 h 20 min','200','Varanasi Airport','2','33','6302');
+
+Insert into flight_details (flightNo,airline,source_place,destination_place,departure,arrival,duration,duration_in_min,airportName,totalStops,availableSeats,fare
+) values ('EA-123','Etihad Airways','Hydrebad','Kochi','13:22:33','17:28:59','4 h 6 min','246','Hydrebad Airport','3','34','5056');
+
+Insert into flight_details (flightNo,airline,source_place,destination_place,departure,arrival,duration,duration_in_min,airportName,totalStops,availableSeats,fare
+) values ('QA-124','Qatar Airways','Tiruchirapalli','Amritsar','14:44:04','17:30:07','2 h 46 min','166','Tiruchirapalli Airport','2','29','6847');
+
+
+Insert into flight_details (flightNo,airline,source_place,destination_place,departure,arrival,duration,duration_in_min,airportName,totalStops,availableSeats,fare
+) values ('QA-123','Qatar Airways','Guwahati','Kolkata','12:02:02','18:12:44','6 h 10 min','370','Guwahati Airport','2','57','6201');
+Insert into flight_details (flightNo,airline,source_place,destination_place,departure,arrival,duration,duration_in_min,airportName,totalStops,availableSeats,fare
+) values ('AAI-123','AirAsia India.','Chennai','Pune','14:27:14','19:49:39','5 h 22 min','322','Chennai Airport','3','25','8948');
+
+Insert into flight_details (flightNo,airline,source_place,destination_place,departure,arrival,duration,duration_in_min,airportName,totalStops,availableSeats,fare
+) values ('GA-123','GoAir.','Calicut','Srinagar','14:39:49','19:57:30','5 h 17 min','317','Calicut Airport','0','45','1262');
+
+Insert into flight_details (flightNo,airline,source_place,destination_place,departure,arrival,duration,duration_in_min,airportName,totalStops,availableSeats,fare
+) values ('SJ-123','SpiceJet.','Bangalore','Thiruvananathapuram','14:01:30','19:41:45',' 5 h 40 min','340','Bangalore Airport','0','89','1841');
+
+Insert into flight_details (flightNo,airline,source_place,destination_place,departure,arrival,duration,duration_in_min,airportName,totalStops,availableSeats,fare
+) values ('TJ-124','TruJet','Srinagar','Calicut','12:28:18','17:31:21',' 5 h 3 min','303','Srinagar Airport','2','5','1850');
+
+
+Insert into flight_details (flightNo,airline,source_place,destination_place,departure,arrival,duration,duration_in_min,airportName,totalStops,availableSeats,fare
+) values ('SAS-123','Saudi Airlines saudia','Goa','Lucknow','13:43:55','17:51:35',' 5 h 7 min','307','Goa Airport','2','39','3900');
+    
+Insert into flight_details (flightNo,airline,source_place,destination_place,departure,arrival,duration,duration_in_min,airportName,totalStops,availableSeats,fare
 ) values ('IG-123','IndiGo','Ahmedabad','Varanasi','14:43:48','18:35:57','3 h 52 min','232','Ahmedabad Airport','1','45','8756');
 
 Insert into flight_details (flightNo,airline,source_place,destination_place,departure,arrival,duration,duration_in_min,airportName,totalStops,availableSeats,fare
